43b96d33af710eb1ca840a04c70eed253242f5ca
umangaisyahhanum <umangnalasava@gmail.com>
2020-12-06 22:52:09 +0700

    test

--- src/screen/home.js
+++ src/screen/home.js
@@ -2,5 +2,5 @@ import React from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
 export default HomeScreen = ({navigation}) => {
-    return <Text>This is Home Page</Text>;
+    return <Text>This is Home Page test</Text>;
 }
